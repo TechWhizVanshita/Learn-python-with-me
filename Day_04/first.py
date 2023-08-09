ac02d0f08833cae554bd7162ed964d88c0256760
vanshita80048 <vanshita479@gmail.com>
2023-08-09 15:03:16 +0530

    python

---
+++ Day_04/first.py
@@ -0,0 +1,9 @@
+print("Hello Vanshita" , 1063)
+print("Bye")
+print(95+40+25)
+
+#Quick Quiz
+
+#Write a program to print a poem in Python. 
+print("Leaving home behind, they ventured anew,\nChasing lofty dreams, with hearts so true.\nThrough struggles and strife, their journey they chart,\nCrafting their stories, with passion in every part.")
+
---
+++ Day_05/comments.py
@@ -0,0 +1,10 @@
+"""" This is a multi-line comment.
+It is also used as a multi-line string. Python interpreter doesn't interpret this and not showing this on screen."""
+
+#Escape sequence characters
+
+print("You are a \"girl\" and a good human being.")
+
+
+print("Hey", 7, 8, sep="-", end = "011\n")
+print("Vanshita")
