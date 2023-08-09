7e7208e8aad0eae4a8e7c193a0efcd8f1c803d72
vanshita80048 <vanshita479@gmail.com>
2023-08-09 16:29:02 +0530

    python

---
+++ Day_11/strings.py
@@ -0,0 +1,22 @@
+name = "Vanshita"
+friend = "Guru"
+anotherFriend = 'Garvi'
+apple = '''He said, 
+Hi Harry
+hey I am good
+"I want to eat an apple'''
+ 
+print("Hello, " + name)
+# print(apple) 
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+# print(name[8]) # Throws an error
+print("Lets use a for loop\n")
+for character in name:
+    print(character)
