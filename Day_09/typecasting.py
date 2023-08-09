ad784b6616e06471cb88706455b8bc7bc4546a48
vanshita80048 <vanshita479@gmail.com>
2023-08-09 16:17:15 +0530

    python

---
+++ Day_09/typecasting.py
@@ -0,0 +1,11 @@
+a = "1"
+b = "2"
+
+print(int(a) + int (b))
+
+#Implicit typecasting
+c = 2.0
+d = 3
+add2 = c + d
+print(add2)
+print(type(add2))
---
+++ Day_10/user_inp.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+print("My name is ",name)
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+print(a + b)
+print(int(a) + int(b))
+
