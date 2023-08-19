1623f5f24a2da4f1839efab3ec4192b450adea5b
vanshita80048 <vanshita479@gmail.com>
2023-08-19 12:15:45 +0530

    python

---
+++ Day_14/if_else.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your age: "))
+print("Your age is ",age)
+
+
+#Conditional operators
+# < , > ,<= ,>= ,== , !=
+
+if(age>18):
+    print("Yes, You can drive.")
+
+else:
+    print("No, You can't drive.")
+    
+    
