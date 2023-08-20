417fb81f8177afa1b96314b28beee725f18fcdaf
vanshita80048 <vanshita479@gmail.com>
2023-08-20 18:20:36 +0530

    python

--- Day_17/for_loop.py
+++ Day_17/for_loop.py
@@ -1,2 +1,26 @@
-for i in range(20):
-    print(i+1)
+#for in list
+
+students = ["anubha","tanisha","vanshita"]
+for y in students:
+    print(y)
+    
+#Looping through a string
+
+for x in "Vanshita":
+    print(x)
+    
+
+#The break statement
+
+fruits = ["Guava","Cherry","Tomato"];
+for x in fruits:
+    print(x)
+    if x == "Cherry":
+        break
+    
+    
+pickle = ["mango","carrot","garlic"];
+for p in pickle:
+    if p == "carrot":
+        break
+    print(p)
