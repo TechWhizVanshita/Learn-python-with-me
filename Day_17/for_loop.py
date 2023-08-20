f118aa38f64b381c8903a7dc9c8cd31468277a5a
vanshita80048 <vanshita479@gmail.com>
2023-08-20 18:24:19 +0530

    python

--- Day_17/for_loop.py
+++ Day_17/for_loop.py
@@ -24,3 +24,23 @@ for p in pickle:
     if p == "carrot":
         break
     print(p)
+    
+    
+#The continue statement
+
+name = ["Ram","Anuj","Anup","Gita","Shiv"];
+for n in name:
+    if n == "Anup":
+        continue
+    print(n)
+    
+#The range() function
+
+#print values from 0 to 9
+for x in range(0,10):
+    print(x)
+ 
+#print values from 1 to 10   
+for a in range(1,11):
+    print(a)
+    
