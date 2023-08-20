c594599c8887effb9903086f170399f31532c2d5
vanshita80048 <vanshita479@gmail.com>
2023-08-20 18:39:36 +0530

    python

--- Day_17/for_loop.py
+++ Day_17/for_loop.py
@@ -43,4 +43,39 @@ for x in range(0,10):
 #print values from 1 to 10   
 for a in range(1,11):
     print(a)
+   
+   
+for b in range(1,31,1):
+    print(b) 
     
+#Else in for loop
+
+for x in range(2,8):
+    print(x)   
+else:
+    print("Finished!")
+    
+#else block not be executed if there is a break statement
+
+for c in range(4):
+    if c == 3:
+        break
+    print(c)
+else:
+    print("Finished!")
+    
+#Nested loops
+
+adj = ["red","big","tasty"]
+fruits = ["apple","cherry","guava"]
+
+for x in adj:
+    for y in fruits:
+        print(x ,y)
+        
+#The pass statement
+
+#for loop can't be empty, and if we hv no content then we use pass statement to avoid any error.
+
+for i in range(0,2):
+    pass
