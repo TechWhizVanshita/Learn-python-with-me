443f383faa9f1924a122ab3ac0a72c5aa73f5695
Vanshita <vanshita.2226cse1063@kiet.edu>
2023-08-21 23:02:22 +0530

    python

--- functions.py
+++ functions.py
@@ -37,3 +37,23 @@ def my_function5(girl1,girl2,girl3):
     print("The youngest girl child is " + girl3)
     
 my_function5(girl1="Aasma",girl2="Rehnuma",girl3="Sidra")
+
+
+#Arbitrary keyword arguments(**kwargs)
+
+def my_function6(**boys):
+    print("His first name is " + boys["fname"])
+
+my_function6(fname = "Ashu", lname = "Soni")
+
+
+#Default parameter value -- If we call the function without argument, then it uses default value.
+
+def my_function7(city = "Jhansi"):
+    print("I am from " + city)
+    
+my_function7("Hyderabad")
+my_function7("Bengaluru")
+my_function7()
+my_function7("Delhi")
+my_function7("Mumbai")
