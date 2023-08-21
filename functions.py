ac4c2559a452e090b73ed69a76a59aaad902cea2
Vanshita <vanshita.2226cse1063@kiet.edu>
2023-08-21 22:40:36 +0530

    python

--- functions.py
+++ functions.py
@@ -1,17 +1,17 @@
 #Create a function
 
-def my_fuction():
+def my_fuction1():
     print("Hello,I am a vanshita.")
     
 #Call a function
 
-my_fuction()
+my_fuction1()
 
 #Arguments
 
-def name_function(fname):
+def my_function2(fname):
     print(fname + " Devi")
     
-name_fuction("Pushpa")
-name_fuction("Seema")
-name_fuction("Pinky")
+my_function2("Pushpa")
+my_function2("Seema")
+my_function2("Pinky")
