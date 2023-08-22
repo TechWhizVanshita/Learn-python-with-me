bbc5eba10001996489b22d389ee3d21b4815716d
Vanshita <vanshita.2226cse1063@kiet.edu>
2023-08-22 21:48:45 +0530

    python

--- Day_22/lists_methods.py
+++ Day_22/lists_methods.py
@@ -3,3 +3,18 @@
 list_1 = ["one","two","three","four"]
 list_1.append("five")
 print(list_1)
+
+
+#extend() - to append elements from another list
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+#in extend() method you can append any type of iterable object.
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
